b5369dd35d027fdfbd22d8c3be96b04e89d65d5f
saltbo <saltbo@foxmail.com>
2022-09-04 08:26:42 +0800

    feat: add global error notification

--- src/components/apps/Settings.vue
+++ src/components/apps/Settings.vue
@@ -11,19 +11,6 @@ const dataSource = ref()
             <a-input></a-input>
         </div>
         <div>
-            <a-card title="存活探测" style="width: 40%">
-                <template #extra><a href="#">more</a></template>
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
-            </a-card>
-            <br>
-            <a-card title="存活探测" style="width: 40%">
-                <template #extra><a href="#">more</a></template>
-                <p>card content</p>
-                <p>card content</p>
-                <p>card content</p>
-            </a-card>
         </div>
     </div>
 </template>
--- src/hooks/app_op.ts
+++ src/hooks/app_op.ts
@@ -1,18 +1,38 @@
-import { message, Modal } from "ant-design-vue";
+import { message, Modal, notification } from "ant-design-vue";
 import { kiaeCfg } from "../libs/config/kiae";
 import { AppApplication, AppServiceApi } from "../libs/kiae";
-import { defineEmits } from "vue";
+import globalAxios from "axios";
+
+globalAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    console.log(error);
+    const title = error.name;
+    let message = error.message;
+    if (error.response.data.message) {
+      message = error.response.data.message;
+    }
+
+    notification.error({
+      message: title,
+      description: message,
+    });
+    return Promise.reject(error);
+  }
+);
 
 const appSvc = new AppServiceApi(kiaeCfg);
 
 export const useAppOperation = () => {
-  const handleAppCreate = async (app: AppApplication, callback) => {
+  const handleAppCreate = async (app: AppApplication, callback: Function) => {
     await appSvc.appServiceCreate(app);
     message.success("创建成功");
     callback();
   };
 
-  const handleAppDelete = (app: AppApplication, callback) => {
+  const handleAppDelete = (app: AppApplication, callback: Function) => {
     Modal.confirm({
       title: "删除应用",
       content: `确定要删除应用 ${app.name} 吗？`,
